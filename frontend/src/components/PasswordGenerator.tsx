59fcb5b0858dbb35eec4f78e1cffb416927c9cd1
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-01 16:55:30 -0400

    Refactor code

--- frontend/src/components/PasswordGenerator.tsx
+++ frontend/src/components/PasswordGenerator.tsx
@@ -12,9 +12,11 @@ const PasswordGenerator = () => {
         <form onSubmit={handleSubmit}>
           <Flex gap={'2'} direction={'column'}>
             <Heading>Pasword Generator</Heading>
+
             <TextField.Root placeholder='How long should it be?'>
               <TextField.Slot></TextField.Slot>
             </TextField.Root>
+
             <CheckboxGroup.Root defaultValue={['']} name='example'>
               <Flex justify={'center'} gap={'6'}>
                 <CheckboxGroup.Item value='1'>Uppercase</CheckboxGroup.Item>
@@ -22,6 +24,7 @@ const PasswordGenerator = () => {
                 <CheckboxGroup.Item value='3'>Specials</CheckboxGroup.Item>
               </Flex>
             </CheckboxGroup.Root>
+            
             <Button>Generate</Button>
           </Flex>
         </form>
