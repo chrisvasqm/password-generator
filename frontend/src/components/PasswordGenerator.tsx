0d4fe7ba1ec97a79c3febb8734977c24a4a0bd4b
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-01 21:28:39 -0400

    Display genaerted password from backend

--- frontend/src/components/PasswordGenerator.tsx
+++ frontend/src/components/PasswordGenerator.tsx
@@ -1,13 +1,22 @@
 import { Box, Button, Card, Checkbox, FormControlLabel, FormGroup, Stack, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import { useState } from 'react';
 
 const PasswordGenerator = () => {
+  const [password, setPassword] = useState('S4mple!@#');
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
+  const handleGenerate = async () => {
+    const response = await axios.post('http://localhost:3000/api/generate-password', {});
+    setPassword(response.data);
+  }
+
   return (
     <Box width={'50%'}>
-      <Card sx={{paddingY: 2, paddingX: 3, boxShadow: 4, borderRadius: 3}}>
+      <Card sx={{ paddingY: 2, paddingX: 3, boxShadow: 4, borderRadius: 3 }}>
         <form onSubmit={handleSubmit}>
           <Stack gap={1} direction={'column'}>
             <Typography fontSize={24} align='center' marginBottom={2}>Pasword Generator</Typography>
@@ -22,13 +31,13 @@ const PasswordGenerator = () => {
               </Stack>
             </FormGroup>
 
-            <Button>Generate</Button>
+            <Button onClick={handleGenerate}>Generate</Button>
           </Stack>
         </form>
       </Card>
 
       <Typography align='center' marginTop={1}>
-        S4mple!@#
+        {password}
       </Typography>
     </Box >
   );
