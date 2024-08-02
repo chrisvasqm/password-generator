1a50c10f784d450fd5eb035370cc17a9a99c3866
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-02 15:57:39 -0400

    Add include uppercase

--- frontend/src/components/PasswordGenerator.tsx
+++ frontend/src/components/PasswordGenerator.tsx
@@ -5,14 +5,17 @@ import { toast } from 'sonner';
 
 const PasswordGenerator = () => {
   const [password, setPassword] = useState('S4mple!@#');
+  const [length, setLength] = useState(8);
+  const [includeUppercase, setIncludeUppercase] = useState(false);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    handleGenerate();
   };
 
   const handleGenerate = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/api/generate-password', {length: -1});
+      const response = await axios.post('http://localhost:3000/api/generate-password', { length, includeUppercase });
       setPassword(response.data);
     } catch (error: unknown) {
       if (error instanceof AxiosError) toast(error.response?.data);
@@ -25,19 +28,36 @@ const PasswordGenerator = () => {
       <Card sx={{ paddingY: 2, paddingX: 3, boxShadow: 4, borderRadius: 3 }}>
         <form onSubmit={handleSubmit}>
           <Stack gap={1} direction={'column'}>
-            <Typography fontSize={24} align='center' marginBottom={2}>Pasword Generator</Typography>
 
-            <TextField id='password-length' type='number' placeholder='How long you want it to be?' variant='outlined' />
+            <Typography
+              fontSize={24}
+              align='center'
+              marginBottom={2}>Pasword Generator</Typography>
+
+            <TextField
+              id='password-length'
+              type='number'
+              variant='outlined'
+              placeholder='How long you want it to be?'
+              onChange={e => setLength(parseInt(e.target.value))} />
 
             <FormGroup>
               <Stack direction={'row'} justifyContent={'space-between'}>
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Uppercase" />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={includeUppercase}
+                      onChange={() => setIncludeUppercase(!includeUppercase)} />}
+                  label="Uppercase" />
+                
                 <FormControlLabel control={<Checkbox />} label="Numbers" />
+                
                 <FormControlLabel control={<Checkbox />} label="Specials" />
               </Stack>
             </FormGroup>
 
-            <Button onClick={handleGenerate}>Generate</Button>
+            <Button type='submit'>Generate</Button>
+
           </Stack>
         </form>
       </Card>
